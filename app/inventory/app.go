80f5dc0bb87d2b1bab29f87a2b56633ac2cb4091
IshlahulHanif <ishlahul.hanif@tokopedia.com>
2023-05-28 23:05:16 +0700

    make request from backend app delayable

--- app/inventory/app.go
+++ app/inventory/app.go
@@ -7,10 +7,12 @@ import (
 	"github.com/IshlahulHanif/poneglyph"
 	"net"
 	"net/http"
+	"time"
 )
 
 var (
-	myIP string
+	myIP        string
+	delaySecond time.Duration
 )
 
 func main() {
@@ -29,15 +31,90 @@ func main() {
 		fmt.Println(poneglyph.GetLogErrorTrace(err))
 	}
 
-	http.HandleFunc("/", handleRequest)
-	err = http.ListenAndServe(":8080", nil)
+	// Create two routers for different ports
+	requestRouter := http.NewServeMux()
+	settingsRouter := http.NewServeMux()
+
+	// register handlers
+	requestRouter.HandleFunc("/", handleBounceRequest)
+	settingsRouter.HandleFunc("/bounce/delay/add", handleAddDelay)
+
+	var errChan = make(chan error)
+
+	// Start the requestRouter server on port 8080
+	go func() {
+		errServer := http.ListenAndServe(":8080", requestRouter)
+		if errServer != nil {
+			errChan <- poneglyph.Trace(errServer)
+		}
+	}()
+
+	// Start the hostManagementRouter server on port 9090
+	go func() {
+		errServer := http.ListenAndServe(":9090", settingsRouter)
+		if errServer != nil {
+			errChan <- poneglyph.Trace(errServer)
+		}
+	}()
+
+	err = <-errChan
 	if err != nil {
 		err = poneglyph.Trace(err)
 		fmt.Println(poneglyph.GetLogErrorTrace(err))
+		// TODO: consume all errChan to avoid memory leak
 	}
+
+	// TODO: make sure to Shutdown the routers gracefully
 }
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
+func handleAddDelay(w http.ResponseWriter, r *http.Request) {
+	var (
+		err        error
+		statusCode int
+	)
+
+	defer func() {
+		if err != nil {
+			HttpError(w, statusCode, err)
+		}
+	}()
+
+	if r.Method != http.MethodPost {
+		err = poneglyph.Trace(errors.New("method not allowed"))
+		statusCode = http.StatusMethodNotAllowed
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	var payload struct {
+		DelaySecond int `json:"delay_second"`
+	}
+
+	err = decoder.Decode(&payload)
+	if err != nil {
+		err = poneglyph.Trace(err, "Failed to decode body")
+		statusCode = http.StatusBadRequest
+		return
+	}
+
+	delaySecond = time.Duration(payload.DelaySecond) * time.Second
+
+	result := map[string]string{
+		"result": "OK",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(result)
+	if err != nil {
+		err = poneglyph.Trace(err, "Error encode body")
+		statusCode = http.StatusInternalServerError
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func handleBounceRequest(w http.ResponseWriter, r *http.Request) {
 	var (
 		err        error
 		statusCode int
@@ -62,6 +139,10 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 		err = poneglyph.Trace(err, "Failed to decode body")
 		statusCode = http.StatusBadRequest
 		return
+	}
+
+	if delaySecond > 0 {
+		time.Sleep(delaySecond)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
