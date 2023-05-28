f2ee02a3f2d5fe48d9b964b2df149cb421dd67cd
IshlahulHanif <ishlahul.hanif@tokopedia.com>
2023-05-28 11:43:22 +0700

    update backend api app to use standard error response

--- app/inventory/app.go
+++ app/inventory/app.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/IshlahulHanif/poneglyph"
 	"net"
@@ -37,32 +38,51 @@ func main() {
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
+	var (
+		err        error
+		statusCode int
+	)
+
+	defer func() {
+		if err != nil {
+			HttpError(w, statusCode, err)
+		}
+	}()
+
 	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
+		err = poneglyph.Trace(errors.New("method not allowed"))
+		statusCode = http.StatusMethodNotAllowed
 		return
 	}
 
 	decoder := json.NewDecoder(r.Body)
-	var payload map[string]interface{}
-	err := decoder.Decode(&payload)
+	var payload interface{}
+	err = decoder.Decode(&payload)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		err = poneglyph.Trace(err, "Failed to decode body")
+		statusCode = http.StatusBadRequest
 		return
 	}
 
-	// TODO: use the pattern from the round robin app
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Host", myIP)
 
 	// Respond with the received JSON payload
-	responseJSON, _ := json.Marshal(payload)
-	_, err = fmt.Fprint(w, string(responseJSON))
+	err = json.NewEncoder(w).Encode(payload)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		err = poneglyph.Trace(err, "Error encode body")
+		statusCode = http.StatusInternalServerError
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
+}
+
+func HttpError(w http.ResponseWriter, statusCode int, err error) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(statusCode)
+	fmt.Println(poneglyph.GetErrorLogMessage(err))
 }
 
 func GetLocalIP() (string, error) {
